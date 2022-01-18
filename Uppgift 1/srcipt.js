f9de235a6fb8b0e1fc36362da1fd4918e44167c8
Simon <simon.habchi@utb.ecutbildning.se>
2022-01-18 21:37:35 +0100

    gjort om hela

--- Uppgift 1/srcipt.js
+++ Uppgift 1/srcipt.js
@@ -1,41 +1,96 @@
+/*My variablers*/
 const form = document.querySelector('#regForm');
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
 
+
+/*Validation for type=text input feilds */
+
 const validateText = (input) => {
-    if(input.value.trim() === '') { 
-      setError(input, 'Name can\'t be empty')
-      return false;
-    }
-    else if(input.value.trim().length < 2) {
-      setError(input, 'Name must be atleast 2 chars long')
-      return false;
-    }
-    else {
-      setSuccess(input)
-      return true;
-    }
+  if(input.value.trim() === '') { 
+    setError(input, 'Name can\'t be empty')
+    return false;
   }
-
-  const validateEmail = email => {
-    let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  
-    if(email.value.trim() === '') {
-      setError(email, 'You need to enter a email address');
-      return false;
-    } 
-    else if(!regEx.test(email.value)) {
-      setError(email, 'Email address is not valid');
-      return false;
-    }
-    else {
-      setSuccess(email)
-      return true;
-    }
+  else if(input.value.trim().length < 2) {
+    setError(input, 'Name must be atleast 2 chars long')
+    return false;
   }
+  else {
+    setSuccess(input)
+    return true;
+  }
+}
+
+/*Validation for type=email input feilds */
 
 
+const validateEmail = email => {
+  let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if(email.value.trim() === '') {
+    setError(email, 'You need to enter a email address');
+    return false;
+  } 
+  else if(!regEx.test(email.value)) {
+    setError(email, 'Email address is not valid');
+    return false;
+  }
+  else {
+    setSuccess(email)
+    return true;
+  }
+}
+
+/* Changeing styles if user put incorrect information */
+
+const setError = (input, textMessage) => {
+  const parent = input.parentElement;
+  parent.classList.add('is-invalid');
+  parent.classList.remove('is-valid');
+  parent.querySelector('.invalid-input').innerText = textMessage;
+}
+
+
+/* Changeing styles if user put correct information */
+
+const setSuccess = input => {
+  const parent = input.parentElement;
+  parent.classList.remove('is-invalid');
+  parent.classList.add('is-valid');
+}
+
+/* Valides user inputs */
+
+const validate = input => {
+  switch(input.type) {
+    case 'text': return validateText(input)
+    case 'email': return validateEmail(input)
+    default:
+      break;
+  }
+}
+
+/* Store information to console log */
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+  e.preventDefault();
+  
+  errors = []
+
+  for(let i = 0; i < form.length; i++) {
+    errors[i] = validate(form[i])
+  }
+  console.log(errors)
+
+  if(!errors.includes(false)) {
+    const user = {
+      id: Date.now().toString(),
+      firstName : firstName.value,
+      lastName : lastName.value,
+      email : email.value
+    }
+    console.log(user);
+  }
+
+})
